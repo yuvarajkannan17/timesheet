b5007a071a05c5dbeb4f7ae71482df68ff2cbb4e
yuvarajkannan17 <yuvarajkannan17@gmail.com>
2024-04-09 13:55:51 +0530

    modify leave

--- src/Components/Employee/Home/EmployeeHome.js
+++ src/Components/Employee/Home/EmployeeHome.js
@@ -1,7 +1,9 @@
-
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import '../../Supervisor/Home/supervisor.css'
 function EmployeeHome() {
-
+    const [isOpenTimesheet, setIsOpenTimesheet] = useState(false);
+    const [isOpenLeaveManagement, setIsOpenLeaveManagement] = useState(false);
     const navigate = useNavigate();
 
     function goToAddTimesheetPage() {
@@ -29,71 +31,38 @@ function EmployeeHome() {
 
                 <div className='ti-home-container'>
 
-                    {/* notification about timesheet */}
-                    <div className="row text-center ti-home-notification">
+                    <div className='left-navigation'>
+                        <div className={`collapse-container mb-3 ${isOpenTimesheet ? 'active' : ''}`}>
+                            <button onClick={() => setIsOpenTimesheet(!isOpenTimesheet)} className="collapse-toggle btn fw-bold">
+                                Timesheet Options
+                            </button>
+                            {isOpenTimesheet && (
+                                <div className="collapse-content ">
+                                    <ul><Link to={'/employee/addtimesheet'}>Add Timesheet</Link></ul>
+                                    <ul><Link to={'/employee/edittimesheet'}>Edit Timesheet</Link></ul>
+                                    <ul><Link to={'/employee/rejecttimesheet'}>Reject Timesheet</Link></ul>
 
-                        <div className="col   mx-5 my-2 p-2 ">Timesheet to be approved</div>
-                        <div className="col  mx-5  my-2 p-2  ">Rejected Timesheets</div>
-
-                    </div>
-                    <div className="row text-center ti-home-notification">
-
-                        <div className="col   mx-5 my-2 p-2 ">Leaves to be approved</div>
-                        <div className="col  mx-5  my-2 p-2  ">Rejected Leave Request</div>
-
-                    </div>
-
-                    {/* timesheet content */}
-                    <div className="row text-center ti-home-content mt-2">
-                        {/* timesheet status */}
-                        <div className="col mx-5 my-2 p-2 ">
-                            <p className='p-2 title'>Your Submitted Timesheet</p>
-                            <div className='body   p-1 text-start'>
-                                <div className='m-4 ti-home-ti-status p-2'>
-                                    <p className=''>Timesheet Period :</p>
-                                    <p className=''>Created On :</p>
-                                    <div className='d-flex justify-content-around flex-wrap '>
-                                        <button className='status-btn p-2 m-2'>Status</button>
-                                        <button className='view-btn p-2 m-2'>View</button>
-                                    </div>
                                 </div>
-                            </div>
+                            )}
                         </div>
-                        {/* navigation pages */}
-                        <div className="col mx-5 my-2 p-2 ">
-                            <p className='p-2  title'>Timesheet</p>
-                            <div className='p-4 body '>
-                                <div className='my-4 '>
-                                    <button type='button' className='btn p-2 select-btn ' onClick={goToAddTimesheetPage}>Add Timesheet</button>
+                        <div className={`collapse-container mb-3 ${isOpenLeaveManagement ? 'active' : ''}`}>
+                            <button onClick={() => setIsOpenLeaveManagement(!isOpenLeaveManagement)} className="collapse-toggle btn fw-bold">
+                                Leave Management
+                            </button>
+                            {isOpenLeaveManagement && (
+                                <div className="collapse-content ">
+                                    <ul><Link to={'/employee/leaverequest'}>Add Timesheet</Link></ul>
+                                    <ul><Link to={''}>Edit Leave Request</Link></ul>
+                                    <ul><Link to={''}>Reject Leave Request</Link></ul>
                                 </div>
-                                <div className='my-4'>
-                                    <button type='button' className='btn p-2 select-btn' onClick={goToEditPage}>Edit Timesheet</button>
-                                </div>
-
-                                <div className='my-4'>
-                                    <button type='button' className='btn p-2 select-btn' onClick={goToViewRejectPage}>View Rejected Timesheet</button>
-                                </div>
-
-
-
-
-                            </div>
+                            )}
                         </div>
-                        <div className="col mx-5 my-2 p-2 ">
-                            <p className='p-2 title'>Leave Management</p>
-                            <div className='p-3 body'>
-
-
-                                <div className='my-4'>
-                                    <button type='button' className='btn p-2 select-btn' onClick={leaveRequest}>Leave Request</button>
-
-                                </div>
-
-
-                            </div>
-                        </div>
-
                     </div>
+
+                    <div className='right-details'></div>
+
+
+
                 </div>
 
 
